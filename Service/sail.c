f2f7ac6147dce675f2f8ea928d1dbc14a8628ecb
loferga <laforgemateo@gmail.com>
2024-12-09 14:55:24 +0100

    patching sail behavior to match expected input with bound output

--- Service/sail.c
+++ Service/sail.c
@@ -2,7 +2,10 @@
 #include "timer.h"
 
 #define MIN_RATIO 0.05
-#define MAX_RATIO 1.0
+#define MAX_RATIO 0.995
+
+#define MIN_ANGLE STRAIGHT_SAIL
+#define MAX_ANGLE BLOCKING_SAIL
 
 TIM_TypeDef * sail_timer;
 char sail_channel;
@@ -16,6 +19,20 @@ void sail_init(TIM_TypeDef * timer, uint8_t channel) {
 	sail_channel = channel;
 	
 	timer_start(timer);
+}
+
+float angle_to_pwm(int angle) {
+    if (angle > MAX_ANGLE) {
+        return MAX_RATIO;
+    }
+
+    if (angle < MIN_ANGLE) {
+			return MIN_RATIO;
+		}
+		
+    float pwm_percentage = MIN_RATIO + ((float)(angle - MIN_ANGLE) / (MAX_ANGLE - MIN_ANGLE)) * (MAX_RATIO - MIN_RATIO);
+
+    return pwm_percentage;
 }
 
 void sail_set_angle(uint16_t sail_angle) {
